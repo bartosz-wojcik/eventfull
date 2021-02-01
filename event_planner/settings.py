e8ad367da56473c34a82c968c5744e7d60e0ffda
Bartosz Wojcik <bartosz.wojcik@mohawkcollege.ca>
2021-02-01 17:36:54 -0500

    trying to get static to work111

--- event_planner/settings.py
+++ event_planner/settings.py
@@ -12,10 +12,14 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import mimetypes
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# for bootstrap
+
+mimetypes.add_type("text/css", ".css", True)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
