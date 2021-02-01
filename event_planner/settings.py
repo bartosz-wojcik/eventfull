d297bc9ea2616608a596d7e1f605b5f6f2a142a2
Bartosz Wojcik <bartosz.wojcik@mohawkcollege.ca>
2021-02-01 17:20:03 -0500

    static test

--- event_planner/settings.py
+++ event_planner/settings.py
@@ -133,12 +133,10 @@ USE_I18N = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR / 'website_pages/', 'static')
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'website_pages/static'),
-)
+
 
 
