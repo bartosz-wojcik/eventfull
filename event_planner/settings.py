60b988162452b690ce93618d4ae219c0eaa4b49b
Bartosz Wojcik <bartosz.wojcik@mohawkcollege.ca>
2021-02-01 00:39:36 -0500

    fuck

--- event_planner/settings.py
+++ event_planner/settings.py
@@ -80,18 +80,23 @@ WSGI_APPLICATION = 'event_planner.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'eventfull-db',
-        'USER' : 'bartzhome@eventfull-db',
-        'PASSWORD': 'Polska222!',
-        'HOST': 'eventfull-db.postgres.database.azure.com',
-        'POST': '5432'
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3' or '/home/site/wwwroot/db.sqlite3',
+#     }
+# }
 
-
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'eventfull-db',
+#         'USER' : 'bartzhome@eventfull-db',
+#         'PASSWORD': 'Polska222!',
+#         'HOST': 'eventfull-db.postgres.database.azure.com',
+#         'POST': '5432'
+#     }
+# }
 
 
 # Password validation
